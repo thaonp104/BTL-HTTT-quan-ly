f1916de6ab2ef367b8654b7b05fca029b305e892
thaonp <nguyenphuongthao12041998@gmail.com>
2020-06-18 22:49:04 +0700

    design controller 2

--- app/Http/Controllers/Employee/StoreManager/BillController.php
+++ app/Http/Controllers/Employee/StoreManager/BillController.php
@@ -7,5 +7,21 @@ use Illuminate\Http\Request;
 
 class BillController extends Controller
 {
-    //
+    //show list bills
+    public function index()
+    {
+
+    }
+
+    //show detail bill by id
+    public function show()
+    {
+
+    }
+
+    //search bill by id, name
+    public function search()
+    {
+
+    }
 }
